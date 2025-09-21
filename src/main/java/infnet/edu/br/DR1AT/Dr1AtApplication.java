49c5d4ffd4d3a00456e24af428b9ada2d20223ba
Gabriel Borel <biel_borel@hotmail.com>
2025-09-21 09:59:59 -0300

    Iniciação do projeto referente ao DR1-AT

---
+++ src/main/java/infnet/edu/br/DR1AT/Dr1AtApplication.java
@@ -0,0 +1,13 @@
+package infnet.edu.br.DR1AT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr1AtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dr1AtApplication.class, args);
+	}
+
+}
